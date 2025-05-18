e9f949f75a541a8f9b9de47db8467e6055a9204b
luqilin <luqilin@4417.com>
2025-05-18 21:20:13 +0800

    update

--- editor/src/utils/layout.ts
+++ editor/src/utils/layout.ts
@@ -1,59 +1,193 @@
-import { StoryNode } from '../types/story'
+import {
+  Node,
+} from '@xyflow/react'
+import { MyNode } from '../types/define'
 
 interface Position {
   x: number
   y: number
 }
 
-// export const calculateNodePositions = (nodes: StoryNode[]): Record<string, Position> => {
-//   const positions: Record<string, Position> = {}
-//   const nodeMap = new Map<string, StoryNode>()
-//   const visited = new Set<string>()
-//   const levels: string[][] = []
+export const caculateNodePositions = (nodes: Node[]): Record<string, Position> => {
+  const VERTICAL_SPACING = 50 // 恢复垂直间距
+  let currentY = 0
 
-//   // 构建节点映射
-//   nodes.forEach(node => nodeMap.set(node.node_id, node))
+  // 获取ReactFlow容器的尺寸
+  const flowContainer = document.querySelector('.react-flow')
+  if (!flowContainer) return {};
+  const containerRect = flowContainer.getBoundingClientRect()
+  const containerCenterX = containerRect.width / 2
 
-//   // 找到所有起始节点（没有其他节点指向它的节点）
-//   const startNodes = nodes.filter(node => 
-//     !nodes.some(n => n.next_node_id === node.node_id)
-//   )
+  // 通过计算choiceNode所有子节点的宽度，来计算父节点的宽度
+  let nodesHasChild: { [key: string]: Node[] } = {};
+  nodes.map((node) => {
+    if (node.type === 'choiceNode') {
+      if (!node.parentId) {
+        return node;
+      }
 
-//   // 使用BFS进行层级遍历
-//   const queue = startNodes.map(node => ({ node, level: 0 }))
-  
-//   while (queue.length > 0) {
-//     const { node, level } = queue.shift()!
-    
-//     if (visited.has(node.node_id)) continue
-//     visited.add(node.node_id)
-    
-//     if (!levels[level]) levels[level] = []
-//     levels[level].push(node.node_id)
-    
-//     if (node.next_node_id) {
-//       const nextNode = nodeMap.get(node.next_node_id)
-//       if (nextNode) {
-//         queue.push({ node: nextNode, level: level + 1 })
-//       }
-//     }
-//   }
+      if (!nodesHasChild[node.parentId]) {
+        nodesHasChild[node.parentId] = [];
+      }
+      nodesHasChild[node.parentId].push(node);
+    }
+  })
 
-//   // 计算每个节点的位置
-//   const HORIZONTAL_SPACING = 250
-//   const VERTICAL_SPACING = 150
+  let choicePostions: { [key: string]: { x: number, y: number } } = {};
+  let parentWidths: { [key: string]: number } = {};
 
-//   levels.forEach((levelNodes, levelIndex) => {
-//     const levelWidth = levelNodes.length * HORIZONTAL_SPACING
-//     const startX = -levelWidth / 2
+  // 计算有子节点的父节点的宽度和子节点的位置
+  Object.keys(nodesHasChild).forEach((parentId) => {
+    const parentNode = nodes.find((node) => node.id === parentId)
+    if (!parentNode) {
+      return
+    }
 
-//     levelNodes.forEach((nodeId, index) => {
-//       positions[nodeId] = {
-//         x: startX + index * HORIZONTAL_SPACING,
-//         y: levelIndex * VERTICAL_SPACING
-//       }
-//     })
-//   })
+    let totoalWidth = 0;
+    let padding = 5;
+    let margin = 10;
 
-//   return positions
-// } 
+    let childNodes = nodesHasChild[parentId];
+
+    let childX = 0;
+    childX += padding;
+
+    childNodes.forEach((childNode) => {
+      const childNodeElement = document.querySelector(`[data-id="${childNode.id}"]`)
+      if (!childNodeElement) {
+        return
+      }
+      const childNodeWidth = childNodeElement.getBoundingClientRect().width
+      totoalWidth += childNodeWidth;
+
+      childNode.position.x = childX;
+
+      choicePostions[childNode.id] = {
+        x: childX + childNodeWidth / 2,
+        y: 60
+      }
+
+      childX += childNodeWidth + margin;
+      childNode.position.y = 60;
+    })
+
+    totoalWidth += padding * 2;
+    totoalWidth += margin * (childNodes.length - 1);
+
+    // console.log("======", parentNode.id, 'totoalWidth', totoalWidth)
+
+    const nodeElement = document.querySelector(`[data-id="${parentNode.id}"]`) as HTMLElement;
+    if (nodeElement) {
+      nodeElement.style.width = totoalWidth + 'px';
+      parentWidths[parentNode.id] = totoalWidth;
+    }
+  })
+
+  let levelNodes: { [key: number]: MyNode[] } = {};
+
+  nodes.forEach((node) => {
+    if (node.type === 'choiceNode') {
+      return node;
+    }
+
+    if (!levelNodes[Number(node.data.level)]) {
+      levelNodes[Number(node.data.level)] = [];
+    }
+    levelNodes[Number(node.data.level)].push(node as MyNode);
+  })
+
+  let nodePostions: { [key: string]: { x: number, y: number } } = {};
+
+  const nodeMargin = 10;
+  // 最大99层
+  for (let i = 0; i < 99; i++) {
+    if (!levelNodes[i]) {
+      continue;
+    }
+
+    if (i == 0) {
+      const nodeElement = document.querySelector(`[data-id="${levelNodes[0][0].id}"]`)
+      if (nodeElement) {
+        const nodeHeight = nodeElement.getBoundingClientRect().height
+        const nodeWidth = nodeElement.getBoundingClientRect().width
+
+        // 计算节点位置，使其中心点与容器中心对齐
+        const newPosition = {
+          x: containerCenterX - nodeWidth / 2,
+          y: currentY
+        }
+
+        nodePostions[levelNodes[0][0].id] = newPosition;
+        currentY += nodeHeight + VERTICAL_SPACING;
+      }
+    } else {
+
+      if (levelNodes[i].length > 1) {
+
+        let totalWidth = 0;
+        let maxHeight = 0;
+
+        levelNodes[i].forEach((node) => {
+          const nodeElement = document.querySelector(`[data-id="${node.id}"]`)
+
+          const nodeHeight = nodeElement!.getBoundingClientRect().height
+          const nodeWidth = nodeElement!.getBoundingClientRect().width
+
+          totalWidth += nodeWidth;
+          maxHeight = Math.max(maxHeight, nodeHeight);
+        })
+
+        totalWidth += nodeMargin * (levelNodes[i].length - 1);
+
+        let preNode = nodes.find((node) => node.id === levelNodes[i][0].data.preIds[0])
+        if (preNode!.type == "choiceNode") {
+          preNode = nodes.find((node) => node.id === preNode?.parentId)
+        }
+
+        const preNodeElement = document.querySelector(`[data-id="${preNode!.id}"]`)
+        let startX = ((nodePostions[preNode!.id] && nodePostions[preNode!.id].x) + preNodeElement!.getBoundingClientRect().width / 2) - totalWidth / 2;
+
+        levelNodes[i].forEach((node) => {
+          const nodeElement = document.querySelector(`[data-id="${node.id}"]`)
+          if (nodeElement) {
+            nodePostions[node.id] = {
+              x: startX,
+              y: currentY
+            }
+
+            startX += nodeElement.getBoundingClientRect().width + nodeMargin;
+          }
+        })
+
+        currentY += maxHeight + VERTICAL_SPACING;
+      } else {
+
+        let preNodeId = levelNodes[i][0].data.preIds[0]
+        let preNode = nodes.find((node) => node.id === preNodeId)
+
+        let centerX = 0;
+        if (preNode!.type == "choiceNode") {
+          centerX = choicePostions[preNode!.id].x - parentWidths[preNode!.parentId!] / 2;
+          const preNodeElement = document.querySelector(`[data-id="${preNode!.parentId}"]`)
+          centerX += ((nodePostions[preNode!.parentId!] && nodePostions[preNode!.parentId!].x)) + preNodeElement!.getBoundingClientRect().width / 2
+        } else {
+          const preNodeElement = document.querySelector(`[data-id="${preNode!.id}"]`)
+          centerX = ((nodePostions[preNodeId] && nodePostions[preNodeId].x) || preNode!.position.x) + preNodeElement!.getBoundingClientRect().width / 2
+        }
+
+        const nodeElement = document.querySelector(`[data-id="${levelNodes[i][0].id}"]`)
+        if (nodeElement) {
+          nodePostions[levelNodes[i][0].id] = {
+            x: centerX - nodeElement.getBoundingClientRect().width / 2,
+            y: currentY
+          }
+
+          currentY += nodeElement.getBoundingClientRect().height + VERTICAL_SPACING;
+        }
+
+      }
+    }
+  }
+
+  return nodePostions;
+}
