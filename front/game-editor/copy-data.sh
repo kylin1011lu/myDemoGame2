4941b0b1d1609243503bbaf864303eabf92b663d
luqilin <luqilin@4417.com>
2025-05-19 23:24:59 +0800

    update

--- front/game-editor/copy-data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# 复制数据文件到游戏编辑器的public目录，以便开发服务器能够直接访问
-mkdir -p /Users/zonst/myDemoGame2/front/game-editor/public/data
-cp /Users/zonst/myDemoGame2/front/data/scene1.json /Users/zonst/myDemoGame2/front/game-editor/public/data/
-echo "数据文件已复制到编辑器的public目录"
--- front/game-editor/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# 安装依赖并启动开发服务器
-cd /Users/zonst/myDemoGame2/front/game-editor
-npm install
-npm run dev
